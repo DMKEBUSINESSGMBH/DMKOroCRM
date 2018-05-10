d92eb9588eb4e79db466dea82f5189b2100af804
René Nitzsche <nitzsche@dmk-ebusiness.de>
2018-05-10 11:47:41 +0200

    Migration fixed

--- src/DMKOroCRM/Bundle/ContactBundle/Migrations/Schema/v1_0/AddBirthdayFlagToContact.php
+++ src/DMKOroCRM/Bundle/ContactBundle/Migrations/Schema/v1_0/AddBirthdayFlagToContact.php
@@ -9,23 +9,27 @@ use Oro\Bundle\EntityBundle\EntityConfig\DatagridScope;
 
 class AddBirthdayFlagToContact implements Migration
 {
+
     public function up(Schema $schema, QueryBag $queries)
     {
-    	$table = $schema->getTable('orocrm_contact');
-		$table->addColumn(
-			'birthdaycal',
-			'integer',
-			[
-				'notnull' => false,
-				'oro_options' => [
-					'extend' => [
-						'is_extend' => true,
-						'owner' => ExtendScope::OWNER_SYSTEM,
-					],
-					'entity' => array('label' => 'Birthday flag'),
-					'datagrid' => array('is_visible' => DatagridScope::IS_VISIBLE_FALSE),
-    			]
-			]
-		);
-	}
+        $table = $schema->getTable('orocrm_contact');
+        if ($table->hasColumn('birthdaycal')) {
+            return;
+        }
+        $table->addColumn('birthdaycal', 'integer', [
+            'notnull' => false,
+            'oro_options' => [
+                'extend' => [
+                    'is_extend' => true,
+                    'owner' => ExtendScope::OWNER_SYSTEM
+                ],
+                'entity' => array(
+                    'label' => 'Birthday flag'
+                ),
+                'datagrid' => array(
+                    'is_visible' => DatagridScope::IS_VISIBLE_FALSE
+                )
+            ]
+        ]);
+    }
 }
